e3c4ab80f4623670bcd451fff6e7f7936fa55797
JericCortez <jericayson2@gmail.com>
2024-09-15 07:51:42 +0000

    initial

---
+++ dataconversion.js
@@ -0,0 +1,15 @@
+let price = "200";
+
+let priceInt = parseInt(price);
+console.log(priceInt); // 200
+
+let priceNumber = Number(price);
+console.log(priceNumber); // 200
+
+let pricePlus = +price;
+console.log(pricePlus); // 200
+
+let distance = 150;
+
+let distanceStr1 = distance.toString();
+console.log(distanceStr1); // "
